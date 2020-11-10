3fb466f69effd5b51e96e963344579cc1f08449d
Tobias G <31646748+Daikalos@users.noreply.github.com>
2020-11-10 11:12:20 +0100

    Update 1

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField, Range(0.0f, 20.0f)]
+    private float m_MouseSensitivity = 1.7f;
+
+    private Camera m_Camera;
+    private Vector3 m_CameraRotation;
+
+    void Awake()
+    {
+        m_Camera = GetComponentInChildren<Camera>();
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * m_MouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * m_MouseSensitivity;
+
+        m_CameraRotation.x -= mouseY;
+        m_CameraRotation.x = Mathf.Clamp(m_CameraRotation.x, -90f, 90f);
+
+        m_Camera.transform.localRotation = Quaternion.Euler(m_CameraRotation);
+        m_Camera.transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField, Range(0.0f, 40.0f)]
+    private float m_Speed = 10.0f;
+    [SerializeField, Range(0.0f, 30.0f)]
+    private float m_JumpHeight = 4.3f;
+    [SerializeField, Range(-30.0f, 0.0f)]
+    private float m_GravityStrength = -20.0f;
+
+    private Rigidbody m_Rigidbody;
+    private CapsuleCollider m_Collider;
+
+    private Vector3 m_Velocity = Vector3.zero;
+
+    private bool m_CanJump = true;
+    private float m_DistToGround = 0.0f;
+
+    private float horiz = 0.0f;
+    private float verti = 0.0f;
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Rigidbody.useGravity = false;
+
+        m_Collider = GetComponent<CapsuleCollider>();
+        m_DistToGround = m_Collider.bounds.extents.y;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (m_CanJump && Input.GetButton("Jump") && IsGrounded())
+        {
+            m_Velocity.y = Mathf.Sqrt(m_JumpHeight * -2.0f * m_GravityStrength);
+            m_CanJump = false;
+        }
+
+        horiz = Input.GetAxis("Horizontal");
+        verti = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDir = Vector3.ClampMagnitude(transform.right * horiz + transform.forward * verti, 1.0f) * m_Speed;
+
+        m_Velocity.y += m_GravityStrength * Time.fixedDeltaTime;
+        m_Velocity = new Vector3(moveDir.x, m_Velocity.y, moveDir.z);
+
+        m_Rigidbody.velocity = m_Velocity;
+
+        if (m_Velocity.y < 0 && IsGrounded())
+        {
+            m_Velocity.y = 0.0f;
+            m_CanJump = true;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, m_DistToGround + 0.1f);
+    }
+}
